9f1b4d117a1e32f8d8177ee2689aaafa2e4957c4
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-06 22:46:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,76 @@
+// ┌─────────────────┐
+// │ Your code here! │	
+// └─────────────────┘
 
+function isAdmin(obj){
+    if(obj.userRole === "ADMIN"){
+     return true
+ }else{
+     return false
+ }
+ }
+ //return obj.userRole === 'ADMIN'
+function getEmail(obj){
+    return obj.firstName[0].toLowerCase() + obj.lastName.toLowerCase() + ".prsvr@gmail.com"
+}
+
+function getPlaylistLength(obj){
+    return obj.songs.length
+}
+
+function getHardestHomework(arr){
+    //lowest score tracker
+    if(arr.length === 0){
+        return ""
+    }
+    let homework = arr[0].name;
+    let scoreAvg = arr[0].averageScore
+    for(let i = 0; i < arr.length; i++){
+        let complete = arr[i]
+        complete.averageScore
+        if(complete.averageScore < scoreAvg){
+            homework = complete.name
+            scoreAvg = complete.averageScore
+        }
+    }return homework
+}
+
+
+function createPhonebook(name, num){
+
+    const phonebook = {}
+    for(let i = 0; i < name.length; i++){
+       phonebook[name[i]] = num[i]
+    }
+    return phonebook
+}
+
+
+// 
+// ┌─────────────────────────────────────┐
+// │ Do not modify code below this line. │
+// └─────────────────────────────────────┘
+
+if (typeof isAdmin === 'undefined') {
+    isAdmin = undefined;
+}
+if (typeof getEmail === 'undefined') {
+    getEmail = undefined;
+}
+if (typeof getPlaylistLength === 'undefined') {
+    getPlaylistLength = undefined;
+}
+if (typeof getHardestHomework === 'undefined') {
+    getHardestHomework = undefined;
+}
+if (typeof createPhonebook === 'undefined') {
+    createPhonebook = undefined;
+}
+
+module.exports = {
+    isAdmin,
+    getEmail,
+    getPlaylistLength,
+    getHardestHomework,
+    createPhonebook
+};
